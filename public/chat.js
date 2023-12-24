54c1417873a2954ab662ff8b92de57bd645e1313
Delcio Capolo <delciocapolo096@gmail.com>
2023-12-25 00:25:47 +0100

    receiving user data and passing it on, to get all chat consersations

--- public/chat.js
+++ public/chat.js
@@ -5,12 +5,48 @@ const username = urlSearch.get("username")
 const room = urlSearch.get("select_room")
 
 socket.emit("select_room", {
-    username,
-    room
-})
+        username,
+        room
+    }, response => {
+        console.log(response)
+    }
+);
 
-console.log(username, room)
+const usernameDiv = document.getElementById("username");
+usernameDiv.innerText = `Hey, ${username} - you are in ${room} room`;
 
+document
+    .getElementById("message_input")
+    .addEventListener("keypress", (event) => {
+        if(event.key === "Enter") {
+            const message = event.target.value;
+            
+            const data = {
+                room,
+                message,
+                username
+            };
+            socket.emit("message", data);
+
+            event.target.value = "";
+        }
+    });
+    
+socket.on("message", ({username, text, createdAt, room}) => {
+    const messageDiv = document.getElementById("messages");
+    messageDiv.innerHTML += `
+    <div id="chat_content">
+        <div class="messages" id="message">
+            <div class="new_message">
+                <label for="" class="form-label">
+                    <strong>${username}</strong>
+                    <span>${text} - ${dayjs(createdAt).format("DD/MM HH:mm")}</span>
+                </label>
+            </div>
+        </div>
+    </div>
+    `
+});
 
 // we'll use emit - to emit an information
 // and on - to listen or receive an information
--- src/websocket.ts
+++ src/websocket.ts
@@ -6,23 +6,63 @@ interface RoomUser {
     room: string;
 }
 
+interface Message {
+    room: string;
+    text: string; // content
+    createdAt: Date;
+    username: string;
+}
+
 const users: Array<RoomUser> = []
+const messages: Message[] = [];
 
 // assim que o cliente se conectar na aplicacao
 // when the client connect to the app
 io.on("connection", (socket) => {
-    socket.on("select_room", ({username, room}) => {
-        // console.log(username, room)
+    socket.on("select_room", ({username, room}, callback) => {
+        // com o join, o usuario sera enviado para sala que ele escolheu
+        // todos os registos(mensagens e outros), serão vistos apenas por esses usuários
+        socket.join(room)
+        
+        const userInRoom = users.find(
+            (user) => user.username === username && user.room === room
+        );
 
+        if(userInRoom) {
+            userInRoom.socket_id = socket.id;
+            return;
+        }
+        
         users.push({
             username, 
             room,
             socket_id: socket.id
-        })
+        });
 
-        console.log(users)
-    })
+        // usando o parametro 'callback' para repassar as mensagens
+        const messagesInRoom = getMessagesRoom(room);
+        callback(messagesInRoom);
+    });
+
+    socket.on("message", ({room, username, message}) => {
+        // saving messages
+        const messageSent: Message = {
+            room,
+            username,
+            text: message,
+            createdAt: new Date()
+        }
+        messages.push(messageSent);
+        
+        // send to users in room
+        io.to(room).emit("message", messageSent);
+    });
 });
 
 // usaremos o io => quando for para nossa aplicacao emitir um ação ou mensagem para aplicação toda
 // e socket(parametro) => quando for mais direcionado para o cliente e não a app todo
+
+function getMessagesRoom(room: string) {
+    const messagesInRoom = messages.filter((message) => message.room === room)
+    return messagesInRoom;
+}
